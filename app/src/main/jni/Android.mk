6657398fcbd7ca4df08e8243fec4a6420020a3b1
Roman Brovko <r.brovko@rollncode.com>
2017-08-30 17:02:43 +0300

    add dependency - libpng

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -6,8 +6,9 @@ LS_CPP=$(subst $(1)/,,$(wildcard $(1)/*.cpp))
 LOCAL_MODULE := droidblaster
 LOCAL_SRC_FILES := $(call LS_CPP,$(LOCAL_PATH))
 LOCAL_LDLIBS := -landroid -llog -lEGL -lGLESv2
-LOCAL_STATIC_LIBRARIES := android_native_app_glue
+LOCAL_STATIC_LIBRARIES := android_native_app_glue png
 
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,android/native_app_glue)
+$(call import-module,libpng)
