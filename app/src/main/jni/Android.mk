87ee18183261e716ea75b5e499f04b87949a1d06
Roman Brovko <r.brovko@rollncode.com>
2017-10-31 12:16:40 +0200

    add use box2d_static

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -6,9 +6,10 @@ LS_CPP=$(subst $(1)/,,$(wildcard $(1)/*.cpp))
 LOCAL_MODULE := droidblaster
 LOCAL_SRC_FILES := $(call LS_CPP,$(LOCAL_PATH))
 LOCAL_LDLIBS := -landroid -llog -lEGL -lGLESv2 -lOpenSLES
-LOCAL_STATIC_LIBRARIES := android_native_app_glue png
+LOCAL_STATIC_LIBRARIES := android_native_app_glue png box2d_static
 
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,android/native_app_glue)
 $(call import-module,libpng)
+$(call import-module,box2d)
